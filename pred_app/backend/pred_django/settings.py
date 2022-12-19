42fd711e9ad69cdac43b14d052f4f29221606ff1
Ryan Painter <82731232+Paintersrp@users.noreply.github.com>
2022-12-18 20:50:48 -0500

    Update settings.py

--- pred_app/backend/pred_django/settings.py
+++ pred_app/backend/pred_django/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = "w4uCmXM^ywl@08R5zLsSR52aZ*S$fD#lH6p01%#$sGyS99Ak5Y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
